31c5543ad1d4fcc3d59eafaf25770895b6997b5e
Gynden <pedrolucasramos6@gmail.com>
2025-11-19 18:15:41 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,8 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-# No Render vamos usar a env DATABASE_URL (Postgres)
-# Localmente, se não tiver, ele usa um SQLite só pra teste
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
-
-# Ajuste necessário porque o Render às vezes manda 'postgres://' em vez de 'postgresql://'
-if DATABASE_URL.startswith("postgres://"):
-    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+DATABASE_URL = os.getenv("DATABASE_URL")  # Render já injeta essa variável
 
 engine = create_engine(DATABASE_URL)
 
